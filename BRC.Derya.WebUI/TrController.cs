a58332a67b07e27d38c1613138ac072f8d1ccdba
kelicihasan <39105093+kelicihasan@users.noreply.github.com>
2019-03-23 10:43:18 +0300

    Update TrController.cs

--- BRC.Derya.WebUI/TrController.cs
+++ BRC.Derya.WebUI/TrController.cs
@@ -15,12 +15,6 @@ namespace BRC.Derya.WebUI.Controllers
         public TrController(IIletisimService iletisimService)
         {
             _iletisimService = iletisimService;
-
-        }
-        // GET: Tr
-        public ActionResult Index()
-        {
-            return View();
         }
         public ActionResult Contact()
         {
@@ -29,36 +23,9 @@ namespace BRC.Derya.WebUI.Controllers
         }
         public ActionResult Contact(string ad,string email,string mesaj,int telefon)
         {
-            var il = _iletisimService.GetAll();
-            IletisimListViewModel model = new IletisimListViewModel{
-                Iletisims = il
-            };
-            _iletisimService.Add(new Iletisim{
-                ad_soyad = ad,
-                email = email,
-                mesaj = mesaj,
-                telefon = telefon
-            });
+            var il = _iletisimService.GetAll();IletisimListViewModel model = new IletisimListViewModel{Iletisims = il};
+            _iletisimService.Add(new Iletisim{ad_soyad = ad,email = email,mesaj = mesaj,telefon = telefon});
             return View(model);
-        }
-        public ActionResult Products()
-        {
-           // ViewBag.Message = "Yapım Aşamasında";
-            return View();
-        }
-        public ActionResult Gallery()
-        {
-            //ViewBag.Message = "Yapım Aşamasında";
-            return View();
-        }
-        public ActionResult Media()
-        {
-         //   ViewBag.Message = "Yapım Aşamasında";
-            return View();
-        }
-        public ActionResult test()
-        {
-            return View();
         }
     }
 }
